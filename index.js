61751059f5d3c842743e79e445bc04932cdb0e72
lobana alarabi <loubanaeng@gmail.com>
2020-02-16 23:11:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ BoardMember.prototype.doCharity = function(){
 };
 
 BoardMember.prototype.releasePressStatement= function(){
-  return "You will see great things from Scube."
+  return "You will see great things from Scuber."
 };
 
 BoardMember.prototype.sayHi= function(){
