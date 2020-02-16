00e9949304c6b841872d7f8e0a68871189a1d1e6
lobana alarabi <loubanaeng@gmail.com>
2020-02-16 23:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function BoardMember(name, homeState, training){
   this.homeState=homeState
   this.training=training
 }
-BoardMember.prototype.veto = function()
+BoardMember.prototype.veto = function(){
+  
+}
